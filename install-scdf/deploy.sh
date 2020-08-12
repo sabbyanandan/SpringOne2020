b000bad3c49050069acd881fd462061c86c736bb
Sabby Anandan <sanandan@pivotal.io>
2020-08-12 14:01:35 -0700

    Swap Rabbit with Kafka

--- install-scdf/deploy.sh
+++ install-scdf/deploy.sh
@@ -94,6 +94,8 @@ if ! helm status "${scdf_release_name}" > /dev/null; then
   echo "Install bitnami/spring-cloud-dataflow scdf_release_name=${scdf_release_name} scdf_namespace=${scdf_namespace}"
 
   helm upgrade --wait -n "${scdf_namespace}" --install "${scdf_release_name}" bitnami/spring-cloud-dataflow \
+    --set kafka.enabled=true \
+    --set rabbitmq.enabled=false \
     --set metrics.enabled=true \
     --set metrics.serviceMonitor.enabled=true \
     --set metrics.serviceMonitor.namespace="${prometheus_namespace}" \
@@ -102,13 +104,11 @@ fi
 echo "A release of bitnami/spring-cloud-dataflow, ${scdf_release_name}, is running on ${scdf_namespace} namespace"
 
 MARIADB_ROOT_PASSWORD=$(kubectl get secret --namespace "${scdf_namespace}" "${scdf_release_name}-mariadb" -o jsonpath="{.data.mariadb-root-password}" | base64 --decode)
-RABBITMQ_PASSWORD=$(kubectl get secret --namespace "${scdf_namespace}" "${scdf_release_name}-rabbitmq" -o jsonpath="{.data.rabbitmq-password}" | base64 --decode)
-RABBITMQ_ERLANG_COOKIE=$(kubectl get secret --namespace "${scdf_namespace}" "${scdf_release_name}-rabbitmq" -o jsonpath="{.data.rabbitmq-erlang-cookie}" | base64 --decode)
 
 helm upgrade --wait -n "${scdf_namespace}" --install "${scdf_release_name}" bitnami/spring-cloud-dataflow \
   --set mariadb.rootUser.password=$MARIADB_ROOT_PASSWORD \
-  --set rabbitmq.auth.password=$RABBITMQ_PASSWORD \
-  --set rabbitmq.auth.erlangCookie=$RABBITMQ_ERLANG_COOKIE \
+  --set kafka.enabled=true \
+  --set rabbitmq.enabled=false \
   --set metrics.enabled=true \
   --set metrics.serviceMonitor.enabled=true \
   --set metrics.serviceMonitor.namespace="${prometheus_namespace}" \
